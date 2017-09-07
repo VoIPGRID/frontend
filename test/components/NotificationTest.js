f5e8ef3f3f64139cccca496c0837ba4874b08085
Tom Offringa <tom.offringa@wearespindle.com>
2017-09-07 16:43:34 +0200

    Testcase for the Notification component, right docs

---
+++ test/components/NotificationTest.js
@@ -0,0 +1,23 @@
+/*eslint-disable */
+
+import { renderComponent, expect } from '../test_helper';
+import Notification from '../../src/components/base/Notification';
+
+describe('Notification', () => {
+    let component;
+
+    beforeEach(() => {
+        const componentState = {base: {notification: {content: 'Test notification', type: 'is-success'}}}
+        component = renderComponent(Notification, null, componentState);
+    });
+
+    it('shows the notification content', () => {
+        expect(component).to.have.text('Test notification');
+    });
+
+    it('has the is-success CSS class', () => {
+        expect(component).to.have.class('notification is-success');
+    });
+});
+
+/*eslint-enable */
